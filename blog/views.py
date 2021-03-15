077da5684b2f36c2b6436034190c1980a64392c1
gkpatil0311 <gkpatil0311@gmail.com>
2021-03-15 21:49:17 +0530

    base.html code completed css and html

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,34 @@ from django.shortcuts import render
 from django.template import Context, loader
 
 # Create your views here.
+ 
+posts = [
+    {
+        'author':'gaurav',
+        'title':'blog post',
+        'content':'first post content',
+        'date_posted':'August 27, 2019'
+    },
+    {
+        'author':'suchita',
+        'title':'blog post',
+        'content':'first post content',
+        'date_posted':'August 27, 2019'
+    }
+]
+
+
 
 def home(request):
     #return HttpResponse('<h1> Blog Home</h1>')
-    return render(request,"blog/home.html")
+    context = {
+        'posts':posts
+    }
+    return render(request,"blog/home.html",context)
 
 def about(request):
     #return HttpResponse('<h1>Blog About</h1>') 
-    return render(request,"blog/about.html")
+    return render(request,"blog/about.html",{'title':'About'})
 
 def login(request):
     #return HttpResponse('<h1>Blog About</h1>') 
